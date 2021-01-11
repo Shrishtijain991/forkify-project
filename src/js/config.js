c8ac0997f71efce39b3b9eaeaba7e85bcc0eb181
Shrishtijain991 <shrishti.jain991@gmail.com>
2021-01-11 16:21:38 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '094a3823-a353-4df2-be9c-129fb0700c1b';
+export const MODAL_CLOSE_SEC = 2.5;
